b26045ab7e031d3b779666f555b945516abb704d
Gunnar Huscroft <gunnarhuscroft@gmail.com>
2019-02-11 15:38:03 -0700

    login component

---
+++ src/components/Login/Alert.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import Login from '../Login/Login'
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
+
+class AlertDialogSlide extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+          Continue to Site
+        </Button>
+        <Dialog
+          open={this.state.open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle id="alert-dialog-slide-title">
+            {"Login Below"}
+          </DialogTitle>
+          <DialogContent>
+           <Login/>
+          </DialogContent>
+         
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default AlertDialogSlide;
